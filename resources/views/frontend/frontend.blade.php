9c3bb9252b1baaa8f34d48a1d3ca66af166b3e0f
DAPELGO <idapelgo@gmail.com>
2024-06-17 10:20:21 +0000

    UPDATE

--- resources/views/frontend/frontend.blade.php
+++ resources/views/frontend/frontend.blade.php
@@ -4,8 +4,8 @@
 <section id="hero" class="d-flex align-items-center">
   <div class="container" style="z-index: 1; text-align:center;">
     <div class="row">
-      <div class="col-lg-6 offset-3">
-          <h1>alertes ou urgences en cours </h1>
+      <div class="col-lg-6 offset-3" style="margin-top: 5rem;">
+          <h1>Alertes ou urgences en cours </h1>
           <!-- ======= Testimonials Section ======= -->
           <div id="testimonials" class="testimonials">
               <div class="container">
@@ -353,23 +353,21 @@
                 <div class="faq-list">
                     <ul>
                       <li data-aos="fade-up">
-                        <i class="bx bx-phone icon-help"></i> <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1">Contact et assistance<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-                        <div id="faq-list-1" class="collapse" data-bs-parent=".faq-list">
+                        <i class="bx bx-phone icon-help"></i> <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-5">Contact et assistance<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                        <div id="faq-list-5" class="collapse" data-bs-parent=".faq-list">
                           <p>
                             Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
                           </p>
                         </div>
                       </li>
-
                       <li data-aos="fade-up" data-aos-delay="100">
-                        <i class="bx bx-book-reader icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-2" class="collapsed">Gestions des recrutements, formations et des intervenants<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-                        <div id="faq-list-2" class="collapse" data-bs-parent=".faq-list">
+                        <i class="bx bx-book-reader icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-6" class="collapsed">Gestions des recrutements, formations et des intervenants<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                        <div id="faq-list-6" class="collapse" data-bs-parent=".faq-list">
                           <p>
                             Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
                           </p>
                         </div>
                       </li>
-
                       <li data-aos="fade-up" data-aos-delay="400">
                         <i class="bx bx-mobile-vibration icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-5" class="collapsed">Outils de communications<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
                         <div id="faq-list-5" class="collapse" data-bs-parent=".faq-list">
@@ -386,15 +384,12 @@
                           </p>
                         </div>
                       </li>
-
                     </ul>
                   </div>
             </div>
         </div>
-
       </div>
     </section><!-- End Frequently Asked Questions Section -->
-
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
         <div class="container">
@@ -404,11 +399,9 @@
             <p>Envoyer un message àl'équipe de CORUS</p>
           </div>
         </div>
-
         <div>
           <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
         </div>
-
         <div class="container">
           <div class="row mt-5">
 
@@ -419,13 +412,11 @@
                   <h4>Localisation:</h4>
                   <p>A108 Trypano, Ouagadougou</p>
                 </div>
-
                 <div class="email">
                   <i class="bi bi-envelope"></i>
                   <h4>Email:</h4>
                   <p>info@corus.bf</p>
                 </div>
-
                 <div class="phone">
                   <i class="bi bi-phone"></i>
                   <h4>Téléphone:</h4>
